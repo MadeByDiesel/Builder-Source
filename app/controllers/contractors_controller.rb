1ea6b264e14ae741f691ec3eb1b0892949a09584
David Alvarado <davidealva@gmail.com>
2014-06-12 13:45:43 -0600

    Scoped views and forms

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,4 +1,5 @@
 class ContractorsController < ApplicationController
+  before_action :set_contractor, only: [:show, :edit, :update, :destroy]
 
   def index
     @contractors = Contractor.all
@@ -8,4 +9,16 @@ class ContractorsController < ApplicationController
     @contractor = Contractor.find(params[:id])
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contractor
+      @contractor = Contractor.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def contractor_params
+      params[:contractor]
+    end
+
 end
+
